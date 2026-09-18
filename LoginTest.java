5a40a64a41d43d3bd508e7e8e7b742185191b473
Ovayo Mxalisa <ovayomxalisa23@gmail.com>
2026-09-18 10:54:44 +0200

    Added JUnit 4 unit tests
    Added JUnit unit tests for Login class.

---
+++ LoginTest.java
@@ -0,0 +1,43 @@
+package loginapplication;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    @Test
+    public void testCorrectUsernameFormat() {
+        Login login = new Login();
+        assertTrue(login.checkUserName("kyl_1"));
+    }
+
+    @Test
+    public void testIncorrectUsernameFormat() {
+        Login login = new Login();
+        assertFalse(login.checkUserName("kyle!!!!!!"));
+    }
+
+    @Test
+    public void testPasswordComplexitySuccess() {
+        Login login = new Login();
+        assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
+    }
+
+    @Test
+    public void testPasswordComplexityFailure() {
+        Login login = new Login();
+        assertFalse(login.checkPasswordComplexity("password"));
+    }
+
+    @Test
+    public void testCellPhoneSuccess() {
+        Login login = new Login();
+        assertTrue(login.checkCellPhoneNumber("+27838968976"));
+    }
+
+    @Test
+    public void testCellPhoneFailure() {
+        Login login = new Login();
+        assertFalse(login.checkCellPhoneNumber("08966553"));
+    }
+}
